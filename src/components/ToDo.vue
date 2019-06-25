3c90f0aaf30d715fae2d14abb9c91f02bb2528a3
tirempbs <32857673+tirempbs@users.noreply.github.com>
2019-06-25 13:31:37 -0400

    Creating todo client repo

---
+++ src/components/ToDo.vue
@@ -0,0 +1,50 @@
+<template lang="html">
+  <div>
+    <form v-on:submit='addTodo($event)'>
+      <input type='text' placeholder='Enter Todo' v-model='newTodo'/>
+      <input type='submit' />
+    </form>
+    <ul>
+      <li v-for='todo in todos' :key='todo._id'>
+        <input type='checkbox' @click='deleteTodo(todo._id)'> {{todo.title}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ToDoAPI from '@/services/ToDoAPI.js'
+export default {
+  data () {
+    return {
+      newTodo: '',
+      todos: []
+    }
+  },
+  mounted () {
+    this.loadTodos()
+  },
+  methods: {
+    async addTodo (evt) {
+      evt.preventDefault() // prevents the form's default action from redirecting the page
+      const response = await ToDoAPI.addTodo(this.newTodo)
+      this.todos.push(response.data)
+      this.newTodo = '' // clear the input field
+    },
+    async loadTodos () {
+      const response = await ToDoAPI.getToDos()
+      this.todos = response.data
+    },
+    async deleteTodo (todoID) {
+      ToDoAPI.deleteTodo(todoID)
+      // remove the array element with the matching id
+      this.todos = this.todos.filter(function (obj) {
+        return obj._id !== todoID
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
